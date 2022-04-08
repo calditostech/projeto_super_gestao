f3819d4f7904a1fcb3830f9762f38f62096a9ffe
Ricardo Ferreira <calditos1988@outlook.com>
2022-04-07 22:28:01 -0300

    implementacoes dos orm 1 para 1 produtos detalhes

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,10 @@ Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(
 
     //produtos
     Route::resource('produto', 'ProdutoController');
+
+    //produtos detalhes
+    Route::resource('produto-detalhe', 'ProdutoDetalheController');
+
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
